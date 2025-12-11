f5f591eb2fe1d034c720c6c3b90302a78219919e
aanyamehta1 <aanyamehta212@gmail.com>
2025-12-10 23:31:09 -0500

    fixed  Django backend structure and restore working API routes

--- swipehire-backend/backend/urls.py
+++ swipehire-backend/backend/urls.py
@@ -1,15 +1,22 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
 
-from jobs.views import job_deck
-from swipes.views import swipe_view, liked_jobs_view
-
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
-    # API endpoints
-    path("api/jobs/deck/", job_deck),
-    path("api/swipes/", swipe_view),
-    path("api/likes/", liked_jobs_view),
+    path('admin/', admin.site.urls),
 ]
